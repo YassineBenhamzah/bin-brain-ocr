bf06bfd8eb496dab15a1077712d97fd2ef39512d
Yassine Benhamzah <yassine.bhmz@hotmail.com>
2026-03-28 14:44:56 +0100

    feat: add test routes, remove node_modules from tracking

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ app.use(express.json());
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
+app.get('/', (req, res) => {
+  res.json({ status: 'non' });
+});
+app.get('/hamza', (req, res) => {
+  res.json({ status: 'hamza' });
+});
+
+
 
 app.listen(3000, () => console.log('OCR service running on port 3000'));
